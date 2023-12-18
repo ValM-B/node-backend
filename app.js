ed4897ab4e8960bb6253218e0eaede5391fd1bfc
ValM-B <valerie.maurice@oclock-school>
2023-12-18 10:36:59 +0100

    creation middleware

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const express = require('express');
 // Création de l'application express
 const app = express();
 
-// fonction qui reçoit la requete et renvoie la réponse
-app.use((req, res) => {
+// next renvoie à la prochaine fonction
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+// fonction qui reçoit la requete et la réponse
+// next renvoie à la prochaine fonction
+app.use((req, res, next) => {
     res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
 });
 
 // Exportation de l'application pour y accéder depuis les autres fichiers du projet (notemment le serveur node)
