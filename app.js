c7d50d8cf8b1cd526e49433d29b0a6ee2b12bd0e
ValM-B <valerie.maurice@oclock-school>
2023-12-18 10:29:37 +0100

    execution app express

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const express = require('express');
 // Création de l'application express
 const app = express();
 
+// fonction qui reçoit la requete et renvoie la réponse
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+});
+
 // Exportation de l'application pour y accéder depuis les autres fichiers du projet (notemment le serveur node)
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // importation du package http de node
 const http = require('http');
 
+// Importation de l'application express
+const app = require('./app');
+
+// on définit le port de l'application
+app.set('port', process.env.PORT || 3000);
+
 // Création du serveur argument req = requete et res = réponse
-const server = http.createServer((req, res) => {
-  res.end('Voilà la réponse duserveur !');
-});
+// const server = http.createServer((req, res) => {
+//   res.end('Voilà la réponse duserveur !');
+// });
+
+// On remplace la fonction par l'application express
+const server = http.createServer(app);
 
 // Si lenvironnement du serveur est défini, on utilise le port défini, sinon on utilise le port 3000
 server.listen(process.env.PORT || 3000);
